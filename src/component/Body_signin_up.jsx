5c9c6f1e4fc46e51a5b47ca5505ff26d20e50af5
hodu26 <gjdhks1212@gmail.com>
2024-11-11 02:17:30 +0900

    update: 로그인, 회원가입 전환 애니메이션 추가
     update rotate animation between signin, signup

--- src/component/Body_signin_up.jsx
+++ src/component/Body_signin_up.jsx
@@ -14,43 +14,39 @@ function LoginRegister() {
       <div className={`card ${isLogin ? "login-size" : "register-size"}`}>
         {isLogin ? (
           <div className="login-form">
-            <h2>Login</h2>
+            <h2 className="sign-title">로그인</h2>
             <div className="input-group">
               <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
+              <input type="email" placeholder="아이디 (이메일)" />
             </div>
             <div className="input-group">
               <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="비밀번호" />
             </div>
-            <a href="#" className="forgot-password">Forgot Password?</a>
-            <button className="btn btn-login">Login</button>
+            <a href="#" className="forgot-password">비밀번호를 잊으셨나요?</a>
+            <button className="btn btn-login">로그인</button>
             <p className="toggle-text">
-              Don't have an account? <span className="toggle" onClick={toggleForm}>Register Now</span>
+              계정이 없으신가요? <span className="toggle" onClick={toggleForm}>회원가입</span>
             </p>
           </div>
         ) : (
           <div className="register-form">
-            <h2>Registration</h2>
+            <h2 className="sign-title">회원가입</h2>
             <div className="input-group">
               <i className="fas fa-user"></i>
-              <input type="text" placeholder="First Name" />
+              <input type="email" placeholder="아이디 (이메일)" />
             </div>
             <div className="input-group">
               <i className="fas fa-user"></i>
-              <input type="text" placeholder="Last Name" />
+              <input type="password" placeholder="비밀번호" />
             </div>
             <div className="input-group">
               <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="비밀번호 확인" />
             </div>
-            <div className="input-group">
-              <i className="fas fa-lock"></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <button className="btn">Register</button>
+            <button className="btn">회원가입</button>
             <p className="toggle-text">
-              Already have an account? <span className="toggle" onClick={toggleForm}>Login Now</span>
+              이미 계정이 있으신가요? <span className="toggle" onClick={toggleForm}>로그인</span>
             </p>
           </div>
         )}
