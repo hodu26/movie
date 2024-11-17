c8017ef0a5522d77bb007dec76293adc1ead44a1
hodu26 <gjdhks1212@gmail.com>
2024-11-18 02:05:48 +0900

    feat: Loading spiner 설정 및 페이지 컴포넌트 동적 로드 설정
     Loading spinner, React.lazy()

--- src/pages/SignInUp.jsx
+++ src/pages/SignInUp.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Header from 'components/Header';
-import Body from 'components/SignInUp/Body_signin_up';
+import LoadingSpinner from 'components/Loading';
 
-const Home = () => {
+// 컴포넌트 동적 로드
+const Body = React.lazy(() => import('components/SignInUp/Body_signin_up'));
+
+const SignInUp = () => {
   return (
     <div className="w-full h-screen mx-auto bg-[#fff]">
       <Header />
+
       <div className="mt-[4rem]">
-        <Body />
+      {/* 로딩 화면 */}
+      <Suspense
+          fallback={
+            <div className="w-full h-screen flex justify-center items-center bg-gray-900">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Body />
+        </Suspense>
       </div>
     </div>
   );
 };
 
-export default Home;
+export default SignInUp;
