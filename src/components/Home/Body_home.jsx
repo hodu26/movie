33bed9daa523cf1cadf497c25ed4681386d3158e
hodu26 <gjdhks1212@gmail.com>
2024-11-21 22:44:15 +0900

    update: hero section에서 현재 상영작 중 하나 랜덤으로 표출

--- src/components/Home/Body_home.jsx
+++ src/components/Home/Body_home.jsx
@@ -16,11 +16,7 @@ const Home = () => {
 
     return (
         <div className="home-container">
-            <HeroSection
-                image={require('assets/image/venom.jpg')}
-                title="베놈: 라스트 댄스"
-                description="환상의 케미스트리의 액션 블록과 그의 심비오트 베놈은 그들을 노리는 정체불명 존재의 추격을 피해 최어드워을 다니게 된다..."
-            />
+            <HeroSection />
 
             <MovieCategory title="인기 영화" tag="popular" genres={genres} />
             <MovieCategory title="최신 영화" tag="now_playing" genres={genres} />
